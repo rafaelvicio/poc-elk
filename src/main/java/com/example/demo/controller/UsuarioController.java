2ccc931b5c1ebb808284c051b0b4dc2f21671f89
Rafael Vicio <rafaelvicio@icloud.com>
2018-09-26 22:35:51 -0300

    Aplicando logs

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class UsuarioController {
 
+    private final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
     @RequestMapping("/usuarios")
     public String GetUsuario() {
+        logger.info("This is an info message");
         return "hahahah";
     }
 
