614c575c99c5cf508a997629a6f5c2432b3146ac
Rafael Vicio <rafaelvicio@icloud.com>
2018-09-30 22:57:25 -0300

    Ajuste e logs

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +12,17 @@ public class UsuarioController {
 
     private final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
 
+    Random rand = new Random();
+
     @RequestMapping("/inicio")
     public String CriaInicioChamada() {
-        logger.info("INICIO CHAMADA");
+        logger.info("INICIO CHAMADA | " + rand.nextInt(50) + 1);
         return "INICIO CHAMADA";
     }
 
     @RequestMapping("/fim")
     public String CriaFimChamada() {
-        logger.info("FIM CHAMADA");
+        logger.info("FIM CHAMADA | " + rand.nextInt(50) + 1);
         return "FIM CHAMADA";
     }
 
